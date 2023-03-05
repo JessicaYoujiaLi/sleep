a8a4fabec5bcd2f232e48665dbd314d35d76d756
GergelyTuri <gt2253@columbia.edu>
2023-03-05 11:55:12 -0500

    adding proto database func

--- base/core.py
+++ base/core.py
@@ -1,12 +1,24 @@
 # created by: @gergelyturi
+from __future__ import print_function
 from google.colab import auth
 import gspread
 from google.auth import default
 from google.colab import drive
 
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from pprint import pprint
 import pandas as pd
 
-class googleDrive:
+
+
+def mount_drive():
+    drive.mount('/gdrive')
+
+
+class GoogleDrive:
 
     def __init__(self):
         self.creds, self._ = default()
@@ -35,10 +47,40 @@ class googleDrive:
             values, columns=values[0]).iloc[1:]
         return df
 
-    def mount_drive(self):
-        drive.mount('/gdrive')
 
 class Mouse:
+
     def __init__(self, name):
-            self.name = name
-    pass
+        self.name = name
+
+    def add_mouse_database(self, path: str) -> int:
+        """
+    Appends a row of values to the first sheet of a Google Sheets document.
+
+    Args:
+        path (str): A list of values to append to the first row of the sheet.
+
+    Returns:
+        int: The number of cells that were updated in the sheet.
+    """
+    credentials, _ = google.auth.default()
+    service = build('sheets', 'v4', credentials=credentials)
+    body = {
+        "range": "A1:C1",
+        "majorDimension": "ROWS",
+        "values": [self].append(path)
+    }
+    try:
+        response = service.spreadsheets().values().append(
+            spreadsheetId='1H8wvotuf1hyx-VHeft5ZvuAH6b_C8t4u_taySKdDneg',
+            range="A1:C1",
+            valueInputOption="RAW",
+            body=body
+        ).execute()
+        # pprint(response)
+        return pprint(response)
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return 0
+pass
