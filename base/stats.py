db4dcb2fa592b96d81fb43a231c91336cbccdb95
gergely <gt2253@cumc.columbia.edu>
2023-03-14 16:07:29 -0400

    adding paired test

--- base/stats.py
+++ base/stats.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # calculating significant cells during sleep
-def ci_difference(awake_arr, nrem_arr, resamples=5000):
+def ci_difference_unpaired(awake_arr, nrem_arr, resamples=5000):
     """
     uses estimation stats to evaluate the difference between two sets of numbers.
     the documentation can be found here: https://acclab.github.io/DABEST-python-docs/tutorial.html
@@ -11,7 +11,7 @@ def ci_difference(awake_arr, nrem_arr, resamples=5000):
     Parameters:
     ===========
     awake_arr: numpy array
-        compied dfof of awake periods
+        compiled dfof of awake periods
     nrem_arr: numpy array
         compiled dfof of NREM periods
     resamples: int
@@ -19,7 +19,6 @@ def ci_difference(awake_arr, nrem_arr, resamples=5000):
 
     Returns:
     ========
-    tuple
     difference: float
         this is the Cohen D effect size between the two arrays
     significance: float
@@ -33,6 +32,40 @@ def ci_difference(awake_arr, nrem_arr, resamples=5000):
     two_groups_unpaired = dabest.load(dabest_df, idx=("awake", "nrem"),
                                       resamples=resamples)
     stats = two_groups_unpaired.cohens_d
+    significance = stats.results['pvalue_students_t'][0] < 0.05
+    difference = stats.results['difference'][0]
+
+    return difference, significance
+
+def ci_difference_paired(awake_arr, nrem_arr, resamples=5000):
+    """
+    uses estimation stats to evaluate the difference between two sets of numbers.
+    the documentation can be found here: https://acclab.github.io/DABEST-python-docs/tutorial.html
+
+    Parameters:
+    ===========
+    awake_arr: numpy array
+        compiled dfof of awake periods
+    nrem_arr: numpy array
+        compiled dfof of NREM periods
+    resamples: int
+        number of resamples to generate the effect size bootstrap
+
+    Returns:
+    ========
+    difference: float
+        this is the Cohen D effect size between the two arrays
+    significance: float
+        the p value for the (paired) student test
+    """
+    dabest_dict = {'awake': awake_arr, 'nrem': nrem_arr}
+    dabest_df = pd.DataFrame.from_dict(dabest_dict, orient='index')
+    dabest_df = dabest_df.transpose()
+
+    # calculating the stats
+    two_groups_paired = dabest.load(dabest_df, idx=("awake", "nrem"),
+                                      resamples=resamples)
+    stats = two_groups_paired.cohens_d
     significance = stats.results['pvalue_students_t'][0] < 0.05
     difference = stats.results['difference'][0]
 
