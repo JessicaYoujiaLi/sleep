f1e049032118fb2983c60164609c8aa44151d2c4
GergelyTuri <gt2253@columbia.edu>
2023-04-30 19:08:12 -0400

    correcting multiple things and adding sig_cell function

--- base/xcorr/xcorr_analysis.py
+++ base/xcorr/xcorr_analysis.py
@@ -1,6 +1,5 @@
 # created by: @gergelyturi
 """functions for cross correlation analysis"""
-from os.path import join
 from pathlib import Path
 import collections
 
@@ -8,38 +7,6 @@ import numpy as np
 import pandas as pd
 
 import scipy.stats as stats
-
-
-def prep_csv(data_dir: str, file_name: str, state: str, direction="Upregulated") -> list:
-    """loads a csv file and returns a list of the significant cells
-    
-    Parameters:
-    ===========
-    data_dir: str
-        path like object pointing to the location of the data
-    file_name: str
-        name of the csv file to load
-    state: str
-        brain state associated with the csv as it appears in 
-        the CSV file
-	direction: str, default="Upregulated"
-		'Upregulated' will pull the upregulated cells,
-		'Downregulated is the downregulated ones
-
-    Return:
-    =======
-    ind_list: list
-    	list of ROI labels
-
-    """
-    sig_cells = pd.read_csv(join(data_dir, file_name))
-    sig_cells.drop(labels='Unnamed: 0', inplace=True, axis=1)
-    sig_cells.rename({'cells': 'roi_label'}, inplace=True, axis=1)
-    sig_cells.set_index('roi_label', inplace=True)
-    ind_list = list(sig_cells[
-        (sig_cells['Direction']==direction) &
-                             (sig_cells['State']=='dfof '+state)].index)
-    return ind_list
 
 def brain_state_filter(velo_eeg_df: pd.DataFrame, states:list) -> tuple:
     """
@@ -69,12 +36,12 @@ def brain_state_filter(velo_eeg_df: pd.DataFrame, states:list) -> tuple:
              (velo_eeg_df['REM']==False) &
              (velo_eeg_df['filtered velo']<0.1))
         nrem = ((velo_eeg_df['NREM']) &
-                (velo_eeg_df['filtered velo']<=0.1))
+                (velo_eeg_df['filtered velo']<0.1))
         rem = ((velo_eeg_df['REM']) &
-               (velo_eeg_df['filtered velo']<=0.1))
+               (velo_eeg_df['filtered velo']<0.1))
         locomotion = ((velo_eeg_df['NREM']==False) &
               (velo_eeg_df['REM']==False) &
-              (velo_eeg_df['filtered velo']>0.1))
+              (velo_eeg_df['filtered velo']>=0.1))
         filters = {'awake':awake,
                   'NREM': nrem,
                   'REM':rem,
@@ -85,10 +52,10 @@ def brain_state_filter(velo_eeg_df: pd.DataFrame, states:list) -> tuple:
              (velo_eeg_df['REM']==False) &
              (velo_eeg_df['filtered velo']<0.1))
         nrem = ((velo_eeg_df['NREM']) &
-                (velo_eeg_df['filtered velo']<=0.1))
+                (velo_eeg_df['filtered velo']<0.1))
         locomotion = ((velo_eeg_df['NREM']==False) &
               (velo_eeg_df['REM']==False) &
-              (velo_eeg_df['filtered velo']>0.1))
+              (velo_eeg_df['filtered velo']>=0.1))
         filters = {'awake': awake,
                   'nrem': nrem,
                   'locomotion':locomotion}
@@ -129,7 +96,7 @@ def data_loader(data_dir: str, data_type: str, mouseID: str,
 
     Retrurns:
     =========
-    dictionary
+    dictionary containing the data
     """
     if data_type != ('dfof' or 'spikes'):
         raise ValueError('data_type must be "dfof" or "spikes"')
@@ -138,11 +105,37 @@ def data_loader(data_dir: str, data_type: str, mouseID: str,
 
     # loading stat results for significantly up and downregulated cells
     # during NREM
-    significant_cells = pd.read_csv(Path(data_loc).joinpath('Significant_paired_DABEST_NREM.csv'))
+    sig_cells = pd.read_csv(Path(data_loc).joinpath('Significant_paired_DABEST_NREM.csv'))
     print(f'number of cells in this recording: {len(significant_cells)}')
 
     # loadig EEG and behavior data
     eeg_velocity = pd.read_csv(Path(data_loc).joinpath('velo_eeg.csv'))
     return { data_type: data,
-            'significant_cells':significant_cells,
+            'significant_cells': sig_cells,
             'eeg_velocity':eeg_velocity}
+
+def significant_cells(cell_data: pd.DataFrame, state: str) -> dict:
+    """Returns the list of significant cells for a given state
+    
+    Parameters:
+    ===========
+    cell_data: pd.DataFrame
+        this is coming from the data_loader function    
+    state: str
+        brain state, e.g. 'NREM', 'REM', 'awake'
+
+    Return:
+    =======
+    ind_list: dict
+    	dict of significant roi_label indices
+    """
+    cell_data.drop(labels='Unnamed: 0', inplace=True, axis=1)
+    cell_data.set_index('roi_label', inplace=True)
+    up_ind_list = list(cell_data[
+        (cell_data['Direction']=='Upregulated') &
+                             (cell_data['State']=='dfof '+state)].index)
+    down_ind_list = list(cell_data[
+        (cell_data['Direction']=='Downregulated') &
+                             (cell_data['State']=='dfof '+state)].index)
+    return {'Upregulated': up_ind_list,
+            'Downregulated': down_ind_list}
