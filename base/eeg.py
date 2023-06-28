d95b5d78045c46a293c9746c9f559c12689750d4
Gergely Turi <gt2253@cumc.columbia.edu>
2023-06-28 17:39:15 -0400

    fix?

--- base/eeg.py
+++ base/eeg.py
@@ -8,7 +8,8 @@ from sleep.base.core import GoogleDrive
 import pandas as pd
 
 def load_eeg_velocity_data(mouse: str, day: str, session:str) -> pd.DataFrame:
-  d_path = Path(GoogleDrive.shared_drive_data_path(), 'Sleep', '2p',
+  drive = GoogleDrive()
+  d_path = Path(drive.shared_drive_data_path(), 'Sleep', '2p',
        'Analysis', 'data', mouse, day, session, 'velo_eeg.csv')
   df = pd.read_csv(d_path)
   return df
