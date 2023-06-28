bc0b892d816e22a029f3b9e23866b4cb2751a1f3
Gergely Turi <gt2253@cumc.columbia.edu>
2023-06-28 17:41:24 -0400

    adding doc string

--- base/eeg.py
+++ base/eeg.py
@@ -8,6 +8,7 @@ from sleep.base.core import GoogleDrive
 import pandas as pd
 
 def load_eeg_velocity_data(mouse: str, day: str, session:str) -> pd.DataFrame:
+  """ Loads the velocity and EEG data for a given mouse, day and session."""
   drive = GoogleDrive()
   d_path = Path(drive.shared_drive_data_path(), 'Sleep', '2p',
        'Analysis', 'data', mouse, day, session, 'velo_eeg.csv')
