62f9d6d2d710c2c55251973587c216a0f253a870
Gergely Turi <gt2253@cumc.columbia.edu>
2024-03-10 14:13:05 -0400

    added new convenient function to google_drive module

--- src/google_drive.py
+++ src/google_drive.py
@@ -10,7 +10,7 @@ import pandas as pd
 from google.auth import default
 from google.colab import auth
 
-BASE_DATA_PATH = "/gdrive/Shareddrives/Turi_lab/Data/Sleep/2p/"
+BASE_DATA_PATH = "/gdrive/Shareddrives/Turi_lab/Data/Sleep/2p/Analysis/data"
 
 
 def useful_datasets():
@@ -26,3 +26,20 @@ def useful_datasets():
 
     # Convert to a DataFrame and render.
     return pd.DataFrame.from_records(rows[1:], columns=rows[0])
+
+
+def return_exp_path(mouse_id: str, day: str, session_id: str):
+    """Return the path to the experiment data.
+
+    Args:
+        mouse_id (str): The mouse ID.
+        day (str): The day of the experiment.
+        session_id (str): The session ID.
+
+    Returns:
+        str: The path to the experiment data on google drive.
+    """
+    try:
+        return join(BASE_DATA_PATH, mouse_id, day, session_id)
+    except Exception as e:
+        return e
